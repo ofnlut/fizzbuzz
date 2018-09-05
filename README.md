8748aaacdc3f7198646bd1df4af5ba4151af9644
Devin Mozee <alonzo.mozee@gmail.com>
2018-09-05 13:12:11 -0500

    Add requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # fizzbuzz
+Fizzbuzz Flask app 
+
+## Prerequisites
+`pip install -r requirements.txt`
+
+## How to run
+`python app.py`
+
+- Server will spin up at http://localhost:34232/fizzbuzz/
+
--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
+"""
+    If the url route is even respond with Fizz,
+    if the route is odd respond with Buzz.
+"""
 @app.route('/fizzbuzz/<int:value_id>')
 def fizz_buzz(value_id):
     if value_id % 2 == 0:
@@ -14,6 +13,7 @@ def fizz_buzz(value_id):
     else:
         return 'Buzz', 200
 
+# Any string route respond with error
 @app.route('/fizzbuzz/<name>')
 def no_fizz_buzz(name):
     return 'Error', 400
